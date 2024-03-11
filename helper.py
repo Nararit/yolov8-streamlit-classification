89be2b03442c8e365c9e608639b2e306bc71fd03
Croz <coras-nararit@hotmail.com>
2024-03-12 01:02:43 +0700

    1

---
+++ helper.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+import time
+import streamlit as st
+import cv2 
+
+import settings
+
+
+def load_model(model_path):
+    """
+    Loads a YOLO object detection model from the specified model_path.
+
+    Parameters:
+        model_path (str): The path to the YOLO model file.
+
+    Returns:
+        A YOLO object detection model.
+    """
+    model = YOLO(model_path)
+    return model
